e6405ef7abd4a5b467a9f54c0c917697db2639f5
id17 <adicahyaludin@gmail.com>
2024-01-29 17:30:30 +0700

    1.0.1 tes

--- ordv-biteship.php
+++ ordv-biteship.php
@@ -16,7 +16,7 @@
  * Plugin Name:       OrangerDev - Biteship
  * Plugin URI:        https://ridwan-arifandi.com
  * Description:       Biteship integration with WooCommerce.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Ridwan Arifandi
  * Author URI:        https://ridwan-arifandi.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ORDV_BITESHIP_VERSION', '1.0.0' );
+define( 'ORDV_BITESHIP_VERSION', '1.0.1' );
 
 define( 'ORDV_BITESHIP_PATH', plugin_dir_path( __FILE__ ) );
 define( 'ORDV_BITESHIP_URI', plugin_dir_url( __FILE__ ) );
